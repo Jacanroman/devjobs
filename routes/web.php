c9ccf4c900bf33ac0d9b279405c1d3e8672df930
Jacanroman <jacanroman@gmail.com>
2021-02-28 20:10:15 +0000

    Redireccion al iniciar sesion

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Rutas de vacantes
+
+Route::get('/vacantes', 'VacanteController@index')->name('vacantes.index');
