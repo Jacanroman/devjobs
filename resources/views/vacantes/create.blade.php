1e01a360cd5dd3b34eba506d2d6a57c6bb23e4de
Jacanroman <jacanroman@gmail.com>
2021-03-04 20:41:39 +0000

    Field Select to chose the category

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -21,7 +21,30 @@
                 name="titulo"/>
         </div>
 
-        {{$categorias}}
+        <div class="mb-5">
+            <label for="categoria" 
+            class="block text-gray-700 text-sm mb-2">
+            Categoria: 
+            </label>
+
+            <select
+                id="categoria"
+                class="block appearance-none w-full border border-gray-200 text-gray-700 rounded leading-tight focus:outline-none focus:border-gray-500 p-3 bg-gray-100"
+                name="categoria"
+            >
+                <option disabled selected>- Seleccion -</option>
+
+                @foreach($categorias as $categoria)
+                    <option value="{{$categoria->id}}">
+                        {{$categoria->nombre}}
+                    </option>
+                    
+                @endforeach
+            </select>
+
+        </div>
+
+        {{--$categorias--}}
 
         <button
             type="submit"
