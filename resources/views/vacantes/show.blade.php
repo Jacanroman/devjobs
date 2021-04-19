49d0b45c3ca6a0d1854ed857d3a4ea41e798c724
Jacanroman <jacanroman@gmail.com>
2021-04-19 09:21:57 +0100

    Mostrando la imagen de la vacante usando Ligthbox2

--- resources/views/vacantes/show.blade.php
+++ resources/views/vacantes/show.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" integrity="sha512-ZKX+BvQihRJPA8CROKBhDNvoc2aDMOdAlcm7TUQY+35XYtrd3yh95QOOhsPDQY9QnKE0Wqag9y38OIgEvb88cA==" crossorigin="anonymous" />
+@endsection
+
 @section('content')
     <h1 class="text-3xl text-center mt-10">{{$vacante->titulo}}</h1>
 
     <div class="mt-10 mb-20 md:flex items-start">
         <div class="md:w-3/5">
             <p class="block text-gray-700 font-bold my-2">
-                Publicado: <span class="font-normal">{{$vacante->created_at->diffForHumans()}}</span>
+                Publicado.: <span class="font-normal">{{$vacante->created_at->diffForHumans()}}</span>
             </p>
             <p class="block text-gray-700 font-bold my-2">
                 Categoria: <span class="font-normal">{{$vacante->categoria->nombre}}</span>
@@ -33,6 +37,10 @@
                     {{$skill}}
                 </p>
             @endforeach
+
+            <a href="../storage/vacantes/{{$vacante->imagen}}" data-lightbox="imagen" data-title="Vacante {{ $vacante->titulo}}">
+                <img src="../storage/vacantes/{{$vacante->imagen}}" class="w-40 mt-10">
+            </a>
             
             <div class="descripcion mt-10 mb-5">
                 {!! $vacante->descripcion !!}
