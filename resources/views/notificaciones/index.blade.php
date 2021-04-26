c2c302054795e708cac9220cf91df729ca200b45
Jacanroman <jacanroman@gmail.com>
2021-04-26 16:14:34 +0100

    Guardando un enlace hacia la vacante en las notificaciones

--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -20,6 +20,17 @@
                         Tienes un nuevo candidato en: 
                         <span class="font-bold">{{$data['vacante']}}</span>
                     </p>
+
+                    <p class="mb-4">
+                        Te escribio: 
+                        <span class="font-bold">{{$notificacion->created_at->diffForHumans()}}</span>
+                    </p>
+
+                    <a href="{{route('candidatos.index', ['id' => $data['id_vacante']])}}"
+                        class="bg-gray-500 p-3 inline-block text-xs font-bold uppercase text-white"
+                    >
+                        Ver Candidato
+                    </a>
                 </li>
                 
             @endforeach
