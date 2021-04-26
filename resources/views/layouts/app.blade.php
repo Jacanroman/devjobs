d94c21ce64770e6d2efe79eb4f2aa3597dfe3c3b
Jacanroman <jacanroman@gmail.com>
2021-04-26 12:47:00 +0100

    mostrar un Numero con el Total de Notificaciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,9 +30,9 @@
         </div>
     @endif
 
-    
+
     <div id="app">
-        <nav class="bg-gray-800 shadow-md py-2">
+        <nav class="bg-gray-800 shadow-md py-6">
             <div class="container mx-auto md:px-0">
                 <div class="flex items-center justify-around">
                     <a class="text-2xl text-white" href="{{ url('/') }}">
@@ -53,6 +53,12 @@
                             @else
                                 
                             <span class="text-gray-300 text-sm pr-4">{{Auth::user()->name }}</span> 
+
+                                <a 
+                                    href=""
+                                    class="bg-gray-500 rounded-full mr-2 px-3 py-2 font-bold text-sm text-white"
+                                >{{Auth::user()->unreadNotifications->count()}}</a>
+
                                 <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
