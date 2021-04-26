7c67f4365bd5c3b4a96ee537b4e58c7514bb4bbf
Jacanroman <jacanroman@gmail.com>
2021-04-26 12:04:26 +0100

    Notificacion y recibir email cuando tienes un nuevo mensaje

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Vacante;
 use App\Models\Candidato;
 use Illuminate\Http\Request;
+use App\Notifications\NuevoCandidato;
 
 class CandidatoController extends Controller
 {
@@ -89,6 +90,9 @@ class CandidatoController extends Controller
             'cv' => $nombreArchivo
         ]);
 
+        $reclutador = $vacante->reclutador;
+        $reclutador->notify(new NuevoCandidato($vacante->titulo));
+        
         return back()->with('estado','Tus datos se enviaron correctamente');
     }
 
