150af56e9b3464efb06c8915194c03951b5c9a41
Jacanroman <jacanroman@gmail.com>
2021-04-27 16:13:18 +0100

    Agregando Sweet Alert

--- resources/js/components/EliminarVacante.vue
+++ resources/js/components/EliminarVacante.vue
@@ -12,7 +12,26 @@ export default{
     props: ['vacanteId'],
     methods:{
        eliminarVacante(){
-           console.log("eliminando...", this.vacanteId);
+           //console.log("eliminando...", this.vacanteId);
+
+        this.$swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                this.$swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+           
        }
     }
 }
