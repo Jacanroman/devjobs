9c6fc2a95d1c74795a0aeb1b16a3f02e4dc5dc70
Jacanroman <jacanroman@gmail.com>
2021-05-03 11:28:47 +0100

    almacenando los cambio en la bbdd

--- resources/views/vacantes/edit.blade.php
+++ resources/views/vacantes/edit.blade.php
@@ -13,9 +13,10 @@
     <h1 class="text-2xl text-center mt-10">Editar Vacante {{$vacante->titulo}}</h1>
 
 
-    <form  action="{{route('vacantes.store')}}" method="POST" class="max-w-lg mx-auto my-10">
+    <form  action="{{route('vacantes.update',['vacante' => $vacante->id])}}" method="POST" class="max-w-lg mx-auto my-10">
         
         @csrf
+        @method('PUT')
 
         <div class="mb-5">
             <label for="titulo" 
